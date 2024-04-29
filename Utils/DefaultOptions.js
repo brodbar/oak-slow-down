15f6d3de6bec14f4d4fe6b4a13e8408b4ddb913e
brodbar <muhammet-149-@hotmail.com>
2024-04-29 15:46:40 +0300

    Update default options

--- Utils/DefaultOptions.js
+++ Utils/DefaultOptions.js
@@ -3,7 +3,7 @@ import { MapStore } from "../Stores/MapStore.js";
 const DefaultOptions =
 {
     WindowMs: 5 * 1e3,
-    DelayMs: (Hits) => 500 + Hits * 100,
+    DelayMs: () => 500,
     DelayAfter: 3,
     Store: new MapStore()
 };
